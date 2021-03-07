de6f2fcd1872a0505d97e5a36047cacb6e77e475
Shriya <>
2021-03-07 13:01:14 -0500

    created a helper method to read the bronze file and set values for the private variables

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -8,6 +8,39 @@ public class USACO{
   private static char[][] silverland;
 
 
+  public static void readfilebronze( String filename){
+    try{
+      File f = new File(filename);
+      Scanner wholefile = new Scanner(f);
+      Scanner line1 = new Scanner (wholefile.nextLine());
+      R = line1.nextInt();
+      C = line1.nextInt();
+      E = line1.nextInt();
+      Nb = line1.nextInt();
+      landb = new int[R][C];
+      instructions = new int[Nb][3];
+      for (int i = 0; i<R; i++){
+        Scanner row = new Scanner (wholefile.nextLine());
+        for (int j = 0; j<C; j++){
+          landb[i][j] = row.nextInt();
+        }
+      }
+
+      for (int i = 0; i<Nb; i++){
+        Scanner row = new Scanner (wholefile.nextLine());
+        for (int j = 0; j<3; j++){
+          instructions[i][j] = row.nextInt();
+        }
+      }
+      System.out.println(R);
+      System.out.println(toString(instructions));
+    }
+    catch(FileNotFoundException e){
+      System.out.println("File not found:"+ filename);
+    }
+  }
+
+
   public static String toString(int[][] array){
     String result = "";
     for (int i = 0; i<array.length; i++){
@@ -19,7 +52,6 @@ public class USACO{
     }
     return result;
   }
-
 
   public static void main (String[] args){
     readfilesilver("silverprob.txt");
