1f521c7926872540c8b347f6cf912a81140867f7
Shriya <>
2021-03-07 12:29:43 -0500

    created a tostring so i can test along the way

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -7,7 +7,20 @@ public class USACO{
   private static int N, M, T, R1, C1, R2, C2;
   private static char[][] silverland;
 
-  
+
+  public static String toString(int[][] array){
+    String result = "";
+    for (int i = 0; i<array.length; i++){
+      for (int j = 0; j<array[0].length; j++){
+        result+=array[i][j];
+        result += " ";
+      }
+      result += "\n";
+    }
+    return result;
+  }
+
+
   public static void main (String[] args){
     readfilesilver("silverprob.txt");
     System.out.println(silver("silverprob.txt"));
