9960bf0ceef19b83f481e5572d944d3ba404e249
Shriya <>
2021-03-14 18:38:40 -0400

    light testing done- need to test more tho

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -66,26 +66,31 @@ public class Quick{
    }
 
    public static void quicksort(int[] data){
-      for (int i=0; i<data.length; i++){
+      /*for (int i=0; i<data.length; i++){
         quickselect(data, data.length-1);
-      }
-      //quicksort (data, 0, data.length-1);
+      }*/
+      quicksort (data, 0, data.length-1);
    }
-   /*public static void quicksort(int[] data, int start, int end){
-      if (end - start > 1){
+   public static void quicksort(int[] data, int start, int end){
+      if (start<end){
         int x = partition (data, start, end);
-        quicksort(data, 0, x);
-        quicksort(data, x, end);
+        quicksort(data, start, x-1);
+        quicksort(data, x+1, end);
       }
 
-   }*/
+   }
 
 
    public static void main (String[] args){
-     int [] test = {12345, 56341, 3461, 1, 3, 2, 12, 4, 23,4 ,324,235,425345,4352,413,3235,2345,234,26547,658,457,65846,8,45653,4534,523,6457,6574,4,54,6,7};
-     //int [] test = {0,1,2,3,4,5,6,7,8,9,12,23,234,2345,2346,12345,12346,12347,12348,12349,13334,13784,734890, 234, 123,3243,235,436,457,3423,42354,635,76473432,423,3123,123,12,343,24};
+     //int [] test = {12345, 56341, 3461, 1, 3, 2, 12, 4, 23,4 ,324,235,425345,4352,413,3235,2345,234,26547,658,457,65846,8,45653,4534,523,6457,6574,4,54,6,7};
+    // int [] test = {0,1,2,3,4,5,6,7,8,9,12,23,234,2345,2346,12345,12346,12347,12348,12349,13334,13784,734890, 234, 123,3243,235,436,457,3423,42354,635,76473432,423,3123,123,12,343,24};
      //int [] test = {10,3,8,2};
+       Random rand = new Random();
+       int [] test = new int [1000000];
+     for (int i = 999999; i>1; i--){
+       test[i]= rand.nextInt() % 1000;
+     }
      quicksort(test);
-     System.out.println(toString(test));
+     System.out.println("done");
    }
 }
