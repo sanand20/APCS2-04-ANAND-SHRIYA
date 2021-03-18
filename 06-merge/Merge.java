6500404e682cbfbc4a16dd4999fada2dfdb7ee11
Shriya <>
2021-03-17 21:48:50 -0400

    started the merge method by creating two arrays for the lo to mid and mid to hi

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,10 +1,22 @@
 public class Merge{
-  public static mergesort(int[] data, int lo, int hi){
+  public static void mergesort(int[] data, int lo, int hi){
     if (lo < hi) {
         int x = lo+ (hi-l)/2;
         sort(arr, lo, x);
         sort(arr, x+ 1, hi);
         merge(arr, lo, x, hi);
+    }
+  }
+  public static void merge(int[] data, int lo, int mid, int hi){
+    int size1 = mid - lo + 1;
+    int size2 = hi - mid;
+    int[] loarr = new int [size1];
+    int[] hiarr = new int [size2];
+    for (int i = 0; i<size1; i++){
+      loarr[i] =  data[lo+i];
+    }
+    for (int i = 0; i<size2; i++){
+      hiarr[j] = data [mid + 1 + j];
     }
   }
 }
