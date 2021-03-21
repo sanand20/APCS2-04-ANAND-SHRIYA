58d034e7a362b28eb3896e9d9a0ee9059991b3ab
Shriya <>
2021-03-21 17:18:51 -0400

    constructor and size done

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -3,8 +3,12 @@ public class MyDeque<E>{
   private int size, start, end;
 
   public MyDeque(){  }
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
+  public MyDeque(int initialCapacity){
+    data = new int [initialCapacity];
+  }
+  public int size(){
+    return size;
+  }
   public String toString(){ }
   public void addFirst(E element){ }
   public void addLast(E element){ }
