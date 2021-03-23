034947ce724511ce9e50c29ceb3dda01fd1afac4
Shriya <>
2021-03-23 17:21:09 -0400

    finished the calc method? need to test

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -6,6 +6,31 @@ public class Calculator{
             *@throws IllegalArgumentException when there are too many or too few operands.
             *        Use the string parameter of your exception to indicate what happened.
             */
-           public static double eval(String s){}
+           public static double eval(String s){
+              Stack<Double> str = new Stack<Double> ();
+              Scanner x = new Scanner(s);
+              while (x.hasNext()) {
+         			    if (x.hasNextDouble()) {
+         				        str.push(x.nextDouble());
+             			}
+                  else {
+         				       Double secNum = str.pop();
+         				       Double firNum = str.pop();
+         				       String op = x.next();
+
+         				       if (op.equals("+")) {
+         					        str.push(firNum + secNum);
+              			   }
+                       else if (op.equals("-")) {
+         					        str.push(firNum - secNum);
+                				}
+                       else if (op.equals("*")) {
+                         str.push(firNum * secNum);
+                       }
+                       else {
+                         str.push(firNum / secNum);
+                       }
+         			   }
+         		  }
+           }
        }
-       
