54db8fe8299c02d7192cfa3dc8225f6800f9a444
Shriya <>
2021-03-28 21:31:46 -0400

    made changes from stack to deque

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -8,37 +8,51 @@ public class Calculator{
             *        Use the string parameter of your exception to indicate what happened.
             */
            public static double eval(String s){
-              Stack<Double> str = new Stack<Double> ();
+              Deque<Double> str = new ArrayDeque<Double> ();
               Scanner x = new Scanner(s);
-              while (x.hasNext()) {
-         			    if (x.hasNextDouble()) {
-         				        str.push(x.nextDouble());
+              try{
+                while (x.hasNext()) {
+                  if (!x.hasNextDouble()){
+                    if (str.size()<2){
+                      throw new IllegalArgumentException("too many or too less operands");
+                    }
+                    Double secNum = str.removeFirst();
+                    Double firNum = str.removeFirst();
+                    String op = x.next();
+
+                    if (op.equals("+")) {
+                       str.addFirst(firNum + secNum);
+                    }
+                    else if (op.equals("-")) {
+                       str.addFirst(firNum - secNum);
+                     }
+                    else if (op.equals("*")) {
+                      str.addFirst(firNum * secNum);
+                    }
+                    else {
+                      str.addFirst(firNum / secNum);
+                    }
+                  }
+                  if (x.hasNextDouble()) {
+         				        str.addFirst(x.nextDouble());
              			}
-                  else {
-         				       Double secNum = str.pop();
-         				       Double firNum = str.pop();
-         				       String op = x.next();
 
-         				       if (op.equals("+")) {
-         					        str.push(firNum + secNum);
-              			   }
-                       else if (op.equals("-")) {
-         					        str.push(firNum - secNum);
-                				}
-                       else if (op.equals("*")) {
-                         str.push(firNum * secNum);
-                       }
-                       else {
-                         str.push(firNum / secNum);
-                       }
-         			   }
          		  }
-              return str.pop();
+                return str.removeFirst();
+            }
+            catch(NoSuchElementException e){
+              throw new IllegalArgumentException("too many or too less operands");
+            }
            }
 
 
-           public static void main (String[] args){
-             String test = "11 3 - 4 + 2.5 *";
-             System.out.println(eval(test));
-           }
+           public static void main(String[] args) {
+      System.out.println(eval("9 9 +")); //18.0
+      System.out.println(eval("10 2.0 +")); //12.0
+      System.out.println(eval("11 3 - 4 + 2.5 *")); //30.0
+      System.out.println(eval("8 2 + 99 9 - * 2 + 9 -")); //893.0
+      System.out.println(eval("1 2 3 4 5 + * - -")); //26.0
+      System.out.println(eval("9 8 -")); //1.0
+
+    }
        }
